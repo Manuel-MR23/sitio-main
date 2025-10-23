614793336d09c01722658351d6a056f72393d6f8
Manuel-MR23 <juan.rocha23@cetis107.edu.mx>
2025-10-22 19:08:01 -0700

    segundo proyexcto fimalizado

--- resources/views/caracteristicas.blade.php
+++ resources/views/caracteristicas.blade.php
@@ -19,4 +19,27 @@
     <li class="list-group-item">🪵 Zonas para picnic y descanso</li>
     <li class="list-group-item">🐕 Espacios pet-friendly</li>
 </ul>
+
+
+<h3 class="mt-4">Galería</h3>
+<div class="row">
+ 
+    <div class="col-md-4 mb-3">
+        <img src="{{ asset('img/pq1.jpeg') }}" alt="Parque - vista 1" class="img-fluid rounded">
+    </div>
+
+ 
+    <div class="col-md-4 mb-3">
+        <img src="{{ asset('img/pq2.jpeg') }}" alt="Parque - vista 2" class="img-fluid rounded">
+    </div>
+
+   
+    <div class="col-md-4 mb-3">
+        <a href="{{ asset('img/pq3.jpeg') }}" target="_blank" rel="noopener">
+            <img src="{{ asset('img/pq3.jpeg') }}" alt="Zona de picnic" class="img-fluid rounded">
+        </a>
+    </div>
+</div>
+
+
 @endsection
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -17,6 +17,6 @@
             El Parque Las Riberas es uno de los lugares más emblemáticos de Culiacán.
             Ideal para disfrutar de paseos, convivir con la familia, practicar deporte y conectarte con la naturaleza.
         </p>
-        <img src="{{ asset('img/parque1.jpg') }}" class="img-fluid rounded shadow mt-4" alt="Parque Las Riberas">
+        <img src="{{ asset('img/pq4.jpeg') }}" class="img-fluid rounded shadow mt-4" alt="Parque Las Riberas">
     </div>
 @endsection
